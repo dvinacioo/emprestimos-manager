c234aab08aa142b783bfa7abeb113bee705d6be3
Davi Inácio <dviinacioo@gmail.com>
2026-06-22 10:07:53 -0300

    Adicona primeiros códigos do js

---
+++ app.js
@@ -0,0 +1,25 @@
+function adicionarEmprestimo(){
+    let nome = document.getElementById("nome").value;
+    let valorEmprestimo = document.getElementById("valor").value;
+    let dataEmprestimo = document.getElementById("data").value;
+    let descricaoEmprestimo = document.getElementById("descricao").value;
+
+    const tabela = document.getElementById("tabelaEmprestimos");
+    
+    tabela.innerHTML += `
+
+            <td>${nome}</td>
+            <td>${valorEmprestimo}</td>
+            <td>${dataEmprestimo}</td>
+            <td>${descricaoEmprestimo}</td>
+            <td><button onclick"marcarPagamento()">✅</button></td>
+        
+    `
+}
+
+function marcarPagamento(){
+    document.getElementById("tabelaEmprestimos").innerHTML = "";
+
+}
+
+
